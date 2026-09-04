e60f61d4051fd23bdc6d3a69299058311ed15850
Aditya <34911958+Aditya671@users.noreply.github.com>
2026-09-05 00:49:40 +0530

    fix: make version symbols explicit re-exports

--- version/__init__.py
+++ version/__init__.py
@@ -1,1 +1,4 @@
-from .version import __version__, __build_date__
+from .version import __build_date__ as __build_date__
+from .version import __version__ as __version__
+
+__all__ = ["__version__", "__build_date__"]
